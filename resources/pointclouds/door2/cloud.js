36dbe14ea219281de811c6a6426ce0ab343fd0d1
Gina <ginastavropoulou@gmail.com>
2016-03-23 16:39:28 +0100

    commit everytcommit everything

---
+++ resources/pointclouds/door2/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 3339206,
+    "boundingBox": {
+        "lx": 664477.385,
+        "ly": 5104089.565000057,
+        "lz": 247.79500000953676,
+        "ux": 664491.26,
+        "uy": 5104103.440000057,
+        "uz": 261.67000000953677
+    },
+    "tightBoundingBox": {
+        "lx": 664477.385,
+        "ly": 5104089.565000057,
+        "lz": 247.79500000953676,
+        "ux": 664484.1400001145,
+        "uy": 5104103.440000057,
+        "uz": 254.555
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.1201610267162323,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
